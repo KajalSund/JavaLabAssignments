292a4a130556913b40b7542523513966ab042984
KajalSund <33478785+KajalSund@users.noreply.github.com>
2018-12-10 15:46:41 +0530

    Create Main.java

---
+++ Multithreading/Main.java
@@ -0,0 +1,19 @@
+
+public class Consumer implements Runnable
+{	Buffer b;
+	public Consumer(Buffer b) 
+	{	this.b = b;
+		//creating and starting the consumer thread
+		Thread t = new Thread(this,"Consumer");
+		t.start();
+	}
+	public void run()
+	{	//consume the value
+		while(true)
+		{	b.get();
+			try
+			{	Thread.sleep(1000);	}
+			catch(Exception e) {}
+		}
+	}
+}
